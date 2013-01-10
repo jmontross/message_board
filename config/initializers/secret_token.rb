427bdbfb1dfe64743cb90dd7a153d6ea57abf83e
joshua montross <joshua.montross@gmail.com>
2013-01-09 17:55:51 -0800

    ads initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessageBoard::Application.config.secret_token = '71feb61179bb462213a781cf858260b1addef63f1f1f2119cd4c302197ce4634cf53007df1586e64f5d7f025e73827299a7dd8cf09497607df32e2b300c9cc31'
