d100d2467a1649e42d39efbe480a73458670eff9
joshua montross <joshua.montross@gmail.com>
2013-01-11 18:15:42 -0800

    ads images, makes display with strftime, removes lots of columns, ads test on model for message truncat, ads message truncate method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :message, :name, :title
+
+  def message_truncated
+    if message.length > 20
+      return self.message[0,20] + "..."
+    else
+      return self.message
+    end
+  end
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "makes new post " do
+    post = Post.new( {:message => "awesome", :name => "foo"})
+    assert post
+  end
+
+
+  test "message truncates " do
+
+    post = Post.new( {:message => "awesome awesome awesome", :name => "foo"})
+    assert post.message_truncated, "awesome awesome..."
+
+  end
+
 end
